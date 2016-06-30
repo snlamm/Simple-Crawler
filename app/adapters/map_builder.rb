b3b64430c767039e884ba16cd5fd214d5b53b5fe
snlamm <shmuel.lamm@gmail.com>
2016-06-30 19:43:43 -0400

    add instance variables to mabuilder and pseudocode build_map method

--- app/adapters/map_builder.rb
+++ app/adapters/map_builder.rb
@@ -3,18 +3,26 @@ require 'open-uri'
 
 class MapBuilder
 
-  # get html from link url
-  # get page name
-  # get page links
-  # get static urls (img, video, etc.)
 
-  attr_accessor :link
+
+  attr_accessor :link, :page_map, :page_name, :html
   def initialize(link)
     @link = link
+    @page_map = {}
+    @page_name
+    @html
+  end
+
+
+  def build_map
+    get_html
+    # get page name
+    # get page links
+    # get static urls (img, video, etc.)
   end
 
   def get_html
-    html = Nokogiri::HTML(open(@link))
+    @html = Nokogiri::HTML(open(@link))
   end
 
 end
