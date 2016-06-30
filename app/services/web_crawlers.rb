0972baa972d5d481a3290c7a03789c3c25207aa2
snlamm <shmuel.lamm@gmail.com>
2016-06-30 19:27:40 -0400

    build out structure of recursive method, pseudocode the methods it calls on

--- app/services/web_crawlers.rb
+++ app/services/web_crawlers.rb
@@ -2,13 +2,30 @@ class WebCrawlers
   attr_accessor :domain_root
   def initialize(domain)
     @domain_root = Link.new(domain)
+    @webmap = {}
+    @all_links = []
+    @ll_links << @domain_root
+    recursive_build
   end
 
+  #get next uncrawled links
+  #return if there is no uncrawled link
+  #get sitemap info for that page
+  # recursive build
   def recursive_build
-    #get next uncrawled links
-    #return if there is no uncrawled link
-    #get sitemap info for that page
-    # recursive build
+    next_link = find_uncrawled
+    return unless next_link
+    extract_link_map(next_link)
+    recursive_build
+  end
+
+  def find_uncrawled
+    #find first uncrawled link
+  end
+
+  def extract_link_map(next_link)
+    #make a mapbuilder
+    #update webcrawler class with mapbuilder info
   end
 
 end
